c00d619d399eb4b36643dac8aef43c72c1d24740
Levi Bowles <leviabowles@gmail.com>
2024-12-25 19:52:33 -0600

    filling out more

--- land_evaluation.py
+++ land_evaluation.py
@@ -4,16 +4,23 @@ class land():
                 crop_revenue_sig,
                 crop_cogs_mu,
                 crop_cogs_sig,
-                non_crop_revenue_mu,
-                non_crop_revenue_sig,
-                non_crop_cogs_mu,
-                non_crop_cogs_sig,
+                non_crop_revenue_mu = 0.0,
+                non_crop_revenue_sig = 0.0,
+                non_crop_cogs_mu = 0.0,
+                non_crop_cogs_sig = 0.0,
                 inflation_mu = 3.0, 
                 inflation_sig = 1.0):
         self.inflation_mean = inflation_mu
         self.inflation_sig = inflation_sig
         self.crop_revenue_mu = crop_revenue_mu
         self.crop_revenue_sig = crop_revenue_sig
+        self.crop_cogs_mu = crop_cogs_mu
+        self.crop_cogs_sig = crop_cogs_sig
+        self.non_crop_revenue_mu = non_crop_revenue_mu
+        self.non_crop_revenue_sig = non_crop_revenue_sig
+        self.non_crop_cogs_mu = non_crop_cogs_mu
+        self.non_crop_cogs_sig = non_crop_cogs_sig
     
-    def annual_evaluator(self):
-        return(self.crop_revenue_mu)
+    def annual_profit(self):
+        profit = (self.crop_revenue_mu + self.non_crop_revenue_mu) - (self.crop_cogs_mu + self.non_crop_cogs_mu)
+        return(profit)
