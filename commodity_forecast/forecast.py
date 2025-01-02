023285119838c89b3d7efefb47c394ca49434b80
Levi Bowles <leviabowles@gmail.com>
2025-01-01 21:37:12 -0600

    new functions

--- commodity_forecast/forecast.py
+++ commodity_forecast/forecast.py
@@ -7,6 +7,11 @@ class commodity_forecast:
     
     def __init__(self, path):
         self.df = pd.read_csv(path)
+        
+    def monthitize(self):
+        self.df['date'] = pd.to_datetime(self.df['date'])
+        self.df['date'] = self.df['date'].dt.to_period('M')
+        self.df = self.df.groupby(['date']).mean()
 
     def auto_arima(self):
         self.aa = pm.auto_arima(self.df['price'], error_action='ignore', seasonal=True, m=12)
--- commodity_forecast/wrapper.py
+++ commodity_forecast/wrapper.py
@@ -1,6 +1,10 @@
 import forecast
+import pandas as pd
 path = '/Users/levi.bowles/Downloads/wheat-prices-historical-chart-data.csv'
 
 mod = forecast.commodity_forecast(path = path)
+
+mod.monthitize()
+
 
 mod.auto_arima()
