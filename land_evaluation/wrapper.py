769b47c326b18ac1276cfa7d238094bb0725d09a
Levi Bowles <leviabowles@gmail.com>
2025-01-31 22:05:51 -0600

    update wrapper for casino

--- land_evaluation/wrapper.py
+++ land_evaluation/wrapper.py
@@ -1,7 +1,9 @@
-import land_evaluation
+import land_evaluation.land_evaluation as land_evaluation
 import statistics
 
 farm = land_evaluation.land(crop_revenue_mu = 120, crop_revenue_sig = 30, crop_cogs_mu = 40, crop_cogs_sig = 10, crop_acres = 100)
+casino = land_evaluation.casino_carlo(farm,years = 10, iterations = 100)
+z = casino.run_iterations()
 
 farm.annual_profit()
 farm.simple_valuator(irr = .03)
@@ -10,4 +12,6 @@ simulator = land_evaluation.land_simulator(farm,3)
 
 simulator.prob_field_value()
 
-profit, value = simulator.prob_multiyear_profit(years = 10)
+profit, value, df = simulator.prob_multiyear_profit(years = 10)
+
+simulator.multiyear_summary()
