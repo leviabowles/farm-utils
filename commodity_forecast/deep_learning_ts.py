fa58da1ea48165713e7a612f30bbc3613467e418
Levi Bowles <leviabowles@gmail.com>
2025-04-30 21:51:08 -0500

    created first commit

--- commodity_forecast/deep_learning_ts.py
+++ commodity_forecast/deep_learning_ts.py
@@ -1,3 +1,9 @@
 import torch 
 import pandas as pd
 
+def data_reader(file):
+    '''
+    A simple method to return a dataframe with the core training data from local flat file.
+    '''
+    df = pd.read_csv(file)
+    return(df)
